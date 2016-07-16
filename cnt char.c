55d86cfeccaaba35d0731e3b8706128488f6b882
dharanipriya <dharuchithu@gmail.com>
2016-07-16 16:53:34 +0530

    Create cnt char.c

---
+++ cnt char.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int len;
+char s[200];
+clrscr();
+printf("\n enter the string:");
+for(i=0;i<n;i++)
+scanf("\n %s",&s[i]);
+len=strlen(s[i]);
+printf("\n num of character in a line",len);
+getch();
+}
