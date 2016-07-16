3b952c15abb5aa37944e1eefeff6d8e2eb12c8b8
dharanipriya <dharuchithu@gmail.com>
2016-07-16 17:00:42 +0530

    Create cnt line.c

---
+++ cnt line.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int cnt=1,len,n;
+char s[100];
+printf("\n string:");
+for(i=0;i<n;i++)
+scanf("\n %s",s[i]);
+len=strlen(s[i]);
+for(i=0;i<len;i++0
+{
+if(a[i]=='\n')
+cnt=cnt+1;
+printf("\n %d",cnt);
+}
+getch();
+}
